4c18206d99f364097bdb5261c0d7da62d7e7623a
Макс <vip.mr000@mail.ru>
2022-06-02 09:48:42 +0300

    add SendMessage

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import sys
+import datetime
+import requests
+import json
+from requests.exceptions import HTTPError
 from PyQt6 import uic, QtCore, QtGui, QtWidgets
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -7,6 +11,22 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('messanger.ui', self)
+        self.pushButton.clicked.connect(self.pushButton_clicked)
+
+    def pushButton_clicked(self):
+        self.SendMessage()
+
+    def SendMessage(self):
+        UserName = self.lineEdit1.text()
+        MessageText = self.lineEdit2.text()
+        TimeStamp = str(datetime.datetime.today())
+        msg = f"{{\"UserName\": \"{UserName}\", \"MessageText\": \"{MessageText}\", \"TimeStamp\": \"{TimeStamp}\"}}"
+        # {"UserName": "RusAl", "MessageText": "Privet na 100 let", "TimeStamp": "2021-03-05T18:23:12"}
+        print("Отправлено сообщение: " + msg)
+        url = self.ServerAdress + "/api/Messanger"
+        data = json.loads(msg) # str to json
+        r = requests.post(url, json=data)
+        # print(r.sttus_code, r.reason)
 
 
 # Press the green button in the gutter to run the script.
